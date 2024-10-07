0b910b68205ca6366f7d6a99625e4fd8b60dd029
HuyLo246 <huylo4660@gmail.com>
2024-10-07 22:19:00 +0700

    website

--- forms/contact.php
+++ forms/contact.php
@@ -1,5 +1,5 @@
 <?php
-  $receiving_email_address = 'huylo4660@gmail.com';
+  $receiving_email_address = 'huylo4660@gmail.com'; // Email to send to
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -10,7 +10,7 @@
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
   
-  $contact->to = $receiving_email_address;
+  $contact->to = $receiving_email_address; // Set recipient email
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
@@ -19,5 +19,5 @@
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+  echo $contact->send(); // Send the email
 ?>
